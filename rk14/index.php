f65f75d59bc7c8816c9449cf14f88cf477f16258
zrm0328 <44760964+zrm0328@users.noreply.github.com>
2018-12-13 08:25:52 +0800

    Add files via upload

---
+++ rk14/index.php
@@ -0,0 +1,14 @@
+<?php
+function FindNumbersWithSum($array, $sum)
+{
+	$array=[];
+	for ($i=0; $i<$array;$i++) 
+	{ 
+		$s=array($array);
+	}
+	return $s;
+
+echo FindNumbersWithSum([1,2,3],6);
+
+
+?>
